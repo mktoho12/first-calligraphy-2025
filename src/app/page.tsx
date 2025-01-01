4f320f31415ff6834bb6e8aa02f4b411a8d80c44
えむけー <mk@mktoho.org>
2025-01-01 16:17:47 +0900

    githubのロゴ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,14 +17,14 @@ export default function Home() {
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/mktoho12/first-calligraphy-2025"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
+            src="/github.svg"
+            alt="Github icon"
             width={16}
             height={16}
           />
